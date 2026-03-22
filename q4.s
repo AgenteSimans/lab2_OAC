15a50c08bd9949ab125746758907fc0a5cb6d9bf
AgenteSimans <gabrielvnrb@hotmail.com>
2026-03-22 20:13:25 -0300

    atividade 4 quase funcionando

--- q4.s
+++ q4.s
@@ -1,20 +1,14 @@
-strl: .string "Hello World"
-
-addi x12,x0,0 #contador
-
+    addi  x12, x0, 0x0      
 loop:
+    lb    x10, 28(x12)         
+    beq   x10, x0, fim        
 
-#//add x13, x11, x12
+    sb    x10, 1024(x0)       
 
-#slli x11,x12,2
-lb x10, 12(x0) #carregar caracter 
-beq x10, x0, fim #vericar fim da string
+    addi  x12, x12, 1         
+    jal   x0, loop
 
-sb x10, 1024(x12) #mostrar string no display 
-
-addi x12,x12,1 #avancar na string
-
-jal x0,loop #gerar repeticao
 fim:
+    halt
+str1: .string "Hello World"   
 
-halt
